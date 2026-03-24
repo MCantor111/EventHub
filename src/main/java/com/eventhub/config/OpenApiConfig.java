f01d191b04dd61aade963e08fb269771636a721a
Cantor Zapalski <matthew.zapalski@edu.sait.ca>
2026-03-23 23:02:33 -0600

    Fixed config for Swagger

--- src/main/java/com/eventhub/config/OpenApiConfig.java
+++ src/main/java/com/eventhub/config/OpenApiConfig.java
@@ -1,19 +1,28 @@
 package com.eventhub.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "EventHub API",
+                version = "1.0",
+                description = "API documentation for EventHub"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI eventHubOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("EventHub API")
-                        .version("1.0")
-                        .description("API for EventHub event management platform"));
-    }
 }
